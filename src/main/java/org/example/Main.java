0c69fe839b9f842843a040b43be0215119cade6f
maxime.leviel@efrei.net <!Re*Q5#2@mNT%yN>
2022-11-09 11:20:58 +0100

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    static List<Double> stack = new ArrayList();
+
+
+
+    public int calculate(String op) {
+        return 0;
+    }
+}
