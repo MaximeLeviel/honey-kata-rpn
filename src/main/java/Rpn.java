ba3f7bb9ac2462f30941ca7cb13ffb03a7758274
maxime.leviel@efrei.net <!Re*Q5#2@mNT%yN>
2022-11-09 11:24:28 +0100

    2. push code

--- src/main/java/Rpn.java
+++ src/main/java/Rpn.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rpn {
+    List<Integer> stack;
+    public Rpn() {
+        stack = new ArrayList();
+    }
+
+    public void push(int number) {
+        stack.add(number);
+    }
 
 }
